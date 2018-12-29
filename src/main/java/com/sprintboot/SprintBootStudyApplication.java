f8ac8f3f1c4d1195e86c6a77ce2c89a5625e7855
wyl <a_wyl1994@163.com>
2018-12-29 14:20:39 +0800

    springboot 空项目

---
+++ src/main/java/com/sprintboot/SprintBootStudyApplication.java
@@ -0,0 +1,14 @@
+package com.sprintboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SprintBootStudyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SprintBootStudyApplication.class, args);
+	}
+
+}
+
